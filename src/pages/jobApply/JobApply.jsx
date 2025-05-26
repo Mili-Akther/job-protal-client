525dfa92d08be46e3e7543c900d1f3cb1b585bf0
Mili Akther <miliakthermilu@gmail.com>
2025-05-27 01:03:30 +0600

    Use custom hook and created job application post api

--- src/pages/jobApply/JobApply.jsx
+++ src/pages/jobApply/JobApply.jsx
@@ -1,63 +1,61 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
-      const {id} = useParams()
-      console.log(id);
+  const { id } = useParams();
+  const {user} = useAuth();
+//   console.log(id, user);
 
-      const submitJonApplication = e =>{
-            e.preventDefault();
-            const form = e.target;
-            const linkedIn = form.linkedIn.value;
-            const github = form.github.value;
-            const resume = form.resume.value;
-            console.log(linkedIn, github, resume);
+  const submitJonApplication = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
+//     console.log(linkedIn, github, resume);
 
-      }
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume
+    }
+  };
 
-      return (
-        <div className="hero bg-base-200 min-h-screen">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Apply now!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-            </div>
-            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-              <form onSubmit={submitJonApplication} className="card-body">
-                <fieldset className="fieldset">
-                  <label className="label">LinkedIn URL</label>
-                  <input
-                    name="linkedIn"
-                    type="url"
-                    className="input"
-                    placeholder="LinkedIn URL"
-                  />
-                  <label className="label">GitHub URL</label>
-                  <input
-                    name="github"
-                    type="url"
-                    className="input"
-                    placeholder="GitHub URL"
-                  />
-                  <label className="label">Resume URL</label>
-                  <input
-                    name="resume"
-                    type="url"
-                    className="input"
-                    placeholder="Resume URL"
-                  />
+  return (
+    <div className="card bg-base-100 w-full shadow-2xl">
+      <h1 className="text-5xl font-bold text-center">Apply job and Good Luck</h1>
+      <form onSubmit={submitJonApplication} className="card-body">
+        <fieldset className="fieldset">
+          <label className="label">LinkedIn URL</label>
+          <input
+            name="linkedIn"
+            type="url"
+            className="input"
+            placeholder="LinkedIn URL"
+          />
+          <label className="label">GitHub URL</label>
+          <input
+            name="github"
+            type="url"
+            className="input"
+            placeholder="GitHub URL"
+          />
+          <label className="label">Resume URL</label>
+          <input
+            name="resume"
+            type="url"
+            className="input"
+            placeholder="Resume URL"
+          />
 
-                  <button className="btn btn-neutral mt-4">Apply</button>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-        </div>
-      );
+          <button className="btn btn-primary mt-4">Apply</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default JobApply;
